4b8f9f2a604d406f2aab235b78793e5eb6bb18ed
Dann2405 <danielsoares0558@gmail.com>
2025-01-17 18:44:13 -0300

    configuração do teclado feita

--- teclado.h
+++ teclado.h
@@ -11,7 +11,20 @@ uint rows[4] = {8, 7, 6, 5}; // linhas do teclado
 char KEY_MAP[16] = { '1', '2', '3', 'A',
                      '4', '5', '6', 'B',
                      '7', '8', '9', 'C',
-                     '*', '0', '#', 'C'}
+                     '*', '0', '#', 'C'};
 
+void setup_keypad()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        gpio_init(columns[i]);
+        gpio_set_dir(columns[i], GPIO_IN);
+        gpio_pull_up(columns[i]);
+
+        gpio_init(rows[i]);
+        gpio_set_dir(rows[i], GPIO_OUT);
+        gpio_put(rows[i], 1);
+    }
+}
 
 #endif // TECLADO_H
