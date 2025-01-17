05ea208700b2fca6b7d522e18ef5a4db24b9f4a6
Wilson Oliveira Lima <oliveiralima@protonmail.com>
2025-01-17 20:25:34 -0300

    feat: implement keypad testing functionality in main loop

--- EmbarcaTech.c
+++ EmbarcaTech.c
@@ -2,24 +2,22 @@
 #include <stdlib.h>
 #include "include/teclado.h"
 
+void test_keypad()
+{
+    setup_keypad();
+
+    while (1)
+    {
+        char key = read_keypad();
+        if (key != '\0')
+        {
+            printf("Tecla pressionada: %c\n", key);
+        }
+    }
+}
+
 int main (){
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    test_keypad();
 
     return 0;
-
 }
