1a8221c6f73c419c910e323a1cb55110f2be4378
Rafael Souza Barbosa <141766772+rafaelsouz10@users.noreply.github.com>
2025-01-18 00:29:58 -0300

    lib stateLeds adicionada e update do diagram.json

--- EmbarcaTech.c
+++ EmbarcaTech.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "include/teclado.h"
+#include "include/stateLeds.h"
 
-void test_keypad()
-{
+void test_keypad(){
+    // Inicializa os pinos de LED e teclado
+    setup_leds();
     setup_keypad();
 
-    while (1)
-    {
+    while (1){
         sleep_ms(50);
-        char key = read_keypad();
-        if (key != '\0')
-        {
+        char key = read_keypad(); // Lê a tecla pressionada
+        if (key != '\0'){        // Se alguma tecla foi pressionada
+            leds(key);          // Aciona o LED correspondente
             printf("Tecla pressionada: %c\n", key);
         }
     }
